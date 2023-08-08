fb22a9ae053f7133e84d0f94459eac492216604e
Alex Harley <alex@alexbh.dev>
2023-08-08 14:18:37 +0200

    feat: constructing orders

--- ts/scripts/submit-order.ts
+++ ts/scripts/submit-order.ts
@@ -3,13 +3,14 @@ import { BigNumber, Wallet, constants, ethers } from "ethers";
 import { optimismGoerli } from "viem/chains";
 
 import dotenv from "dotenv";
+import { hexlify, parseEther } from "ethers/lib/utils";
 dotenv.config();
 
 const OPTIMISM_GOERLI_CHAIN_ID = 420;
 const GOERLI_CHAIN_ID = 5;
 
 const ONE_HOUR = 60 * 60;
-const DEADLINE = Date.now() / 1000 + ONE_HOUR;
+const DEADLINE = Math.floor(Date.now() / 1000 + ONE_HOUR);
 
 async function main() {
   const client = new ethers.providers.JsonRpcProvider(
@@ -21,7 +22,14 @@ async function main() {
   const nonce = await client.getTransactionCount(await wallet.getAddress());
 
   const chainId = optimismGoerli.id;
-  const builder = new DutchOrderBuilder(chainId);
+  const builder = new DutchOrderBuilder(
+    chainId,
+    "0x37f189ebd107c3Ec2447Bdd9387a2B54d6ff9197", // reactor
+    "0x000000000022d473030f116ddee9f6b43ac78ba3" // Permit2 https://docs.uniswap.org/contracts/v3/reference/deployments
+  );
+
+  const startAmount = parseEther("0.01");
+  const endAmount = parseEther("0.009");
 
   const order = builder
     .deadline(DEADLINE)
@@ -30,24 +38,25 @@ async function main() {
     .nonce(BigNumber.from(nonce))
     .input({
       token: constants.AddressZero,
-      startAmount: BigNumber.from("1"),
-      endAmount: BigNumber.from("1"),
+      startAmount: startAmount,
+      endAmount: startAmount,
     })
     .output({
       token: constants.AddressZero,
-      startAmount: BigNumber.from("1"),
-      endAmount: BigNumber.from("1"),
+      startAmount,
+      endAmount,
       recipient: await wallet.getAddress(),
     })
     .validation({
-      additionalValidationData: GOERLI_CHAIN_ID.toString(),
-      additionalValidationContract: "",
+      additionalValidationData: hexlify(GOERLI_CHAIN_ID),
+      additionalValidationContract: constants.AddressZero,
     })
+    .swapper(process.env.ADDRESS!)
     .build();
 
   // Sign the built order
   const { domain, types, values } = order.permitData();
-  const signature = wallet._signTypedData(domain, types, values);
+  const signature = await wallet._signTypedData(domain, types, values);
 
   const serializedOrder = order.serialize();
 
